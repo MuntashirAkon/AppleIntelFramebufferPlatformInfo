cccc02bec8578310dbc95392e68b5b27c2d435c6
Muntashir Al-Islam <muntashir@Muntashirs-MacBook-Pro.local>
2017-07-25 18:09:35 +0600

    Initial Commit

---
+++ ExtractIGPlatformID/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ExtractIGPlatformID
+//
+//  Created by Muntashir Al-Islam on 7/25/17.
+//  Copyright © 2017 Muntashir Al-Islam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
